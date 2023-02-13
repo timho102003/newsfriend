b9e94ae058c6a7c61c128e00f81a83ce0ebef714
timho <timho102003@gmail.com>
2023-02-12 21:34:35 -0800

    add dash authentication

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import dash
+import dash_auth
 
 app = dash.Dash(
     __name__,
@@ -8,5 +9,7 @@ app = dash.Dash(
     ],
     use_pages=True
 )
+VALID_USERNAME_PASSWORD_PAIRS = {"hello": "world"}
+auth = dash_auth.BasicAuth(app, VALID_USERNAME_PASSWORD_PAIRS)
 
 server = app.server
