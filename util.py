2f9d44b03fcdf240c95aeb1d580f2ff4bfc679cb
timho <timho102003@gmail.com>
2023-02-15 22:25:55 -0800

    add utility of checking if image url is accessable

--- util.py
+++ util.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 
 import pytz
+import requests
 
-from config import TIMEZONE
+from config import NO_IMG_URL, TIMEZONE
 
 
 def convert_timezone(
@@ -14,3 +15,11 @@ def convert_timezone(
     desired_timezone = pytz.timezone(desire_timezone)
     desire_time = pytz.utc.normalize(date_time).astimezone(desired_timezone)
     return desire_time
+
+
+def check_img_access(img_url=""):
+
+    response = requests.get(img_url)
+    if not response.status_code == 200:
+        img_url = NO_IMG_URL
+    return img_url
