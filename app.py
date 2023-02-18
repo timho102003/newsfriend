83d86b0bc7594734374b561f76a8a1f7c829cc35
timho <timho102003@gmail.com>
2023-02-18 15:19:37 -0800

    change bootstrap.css

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import dash_auth
 
 app = dash.Dash(
     __name__,
+    external_stylesheets=["assets/bootstrap.morph.css"],
     suppress_callback_exceptions=True,
     meta_tags=[
         {"name": "viewport", "content": "width=device-width, initial-scale=1.0"}
     ],
-    use_pages=True,
+    use_pages=True
 )
 VALID_USERNAME_PASSWORD_PAIRS = {"hello": "world"}
 auth = dash_auth.BasicAuth(app, VALID_USERNAME_PASSWORD_PAIRS)
